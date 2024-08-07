646ebb38db6a7f677d58016c4b39808c1f578f7f
MarcelinoMajerhua <marcelino.majerhua.dev@gmail.com>
2024-08-07 11:40:48 -0500

    update: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://marcelino.majerhua.github.io',
-  base: 'portafolio'
+  site: 'https://MajerhuaAyala.github.io'
 })
